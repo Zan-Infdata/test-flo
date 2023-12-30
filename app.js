52447c16bae7644cda8710ff84416398de25dbfa
zan <zan@infdata.com>
2023-12-30 19:10:15 +0100

    Removing uneccessary libs

--- app.js
+++ app.js
@@ -13,20 +13,6 @@ global.__rootname = path.resolve(__dirname);
 // defining the Express app
 const app = express();
 
-// adding Helmet to enhance your API's security
-app.use(helmet());
-
-
-// using bodyParser to parse JSON bodies into JS objects
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-  
-// enabling CORS for all requests
-app.use(cors());
-
-
-// adding morgan to log HTTP requests
-app.use(morgan('combined'));
 
 
 /**
